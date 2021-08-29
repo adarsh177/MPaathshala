c7e5dcf177ff30a20dd5e71034c6c831d6d9380e
ojas <ojas24012002jain@gmail.com>
2021-08-29 17:57:56 +0530

    UI

--- app/src/main/java/com/affix/myapplication/BasicFunctions.java
+++ app/src/main/java/com/affix/myapplication/BasicFunctions.java
@@ -6,8 +6,9 @@ import android.widget.Spinner;
 
 public class BasicFunctions {
     public static void spinnerGenerator(Spinner spinner, int array, Context context) {
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,array,R.layout.support_simple_spinner_dropdown_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(context,array,R.layout.spinner_bg);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
         spinner.setAdapter(adapter);
     }
 }
